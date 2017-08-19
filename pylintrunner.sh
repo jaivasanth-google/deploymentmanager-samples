9c6c8ca8c631fd8804d271a982164bb8d6d44987
Jai Vasanth <jaivasanth@jaivasanth-macbookpro3.roam.corp.google.com>
2017-08-18 22:14:05 -0700

    pylintrunner outputs to a file

--- pylintrunner.sh
+++ pylintrunner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 function pylintrunner {
-    find . -name *.py | xargs pylint --rcfile=$1
+    find . -name *.py | xargs pylint --rcfile=$1 > $2
     return 0
 }
 
-pylintrunner $1
+pylintrunner $1 $2
